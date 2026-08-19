de2f81cb2827c4e9cb8725cff04a7878918d5a6d
Alejandra Cetina Ucan <cetina.alejandra02@gmail.com>
2026-08-19 07:48:17 -0600

    Cuarta versión - Prueba de cambio

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -1,9 +1,13 @@
 # Prueba de  Python
-# Se añade la variable c 
+# Se añade la variable c
+# Nueva variable E
 a = 10
 b = 30
+e = 1.1
 c = 30*10*5
 print('resultado=', c)
 
-d = c * 100
+## Cambio a factor 200 por Directorio
+
+d = c * 200
 print('resultado total=', d)
